c5ee430c95c532abbe26c3e302d4440e9f156d44
Manasvi <manasvighogale07@gmail.com>
2026-05-07 11:42:04 +0530

    Add files via upload

---
+++ temperature_converter.py
@@ -0,0 +1,28 @@
+
+
+temp = float(input("Enter temperature value: "))
+unit = input("Enter unit (C/F/K): ").upper()
+
+if unit == "C":
+    fahrenheit = (temp * 9/5) + 32
+    kelvin = temp + 273.15
+
+    print("Temperature in Fahrenheit:", fahrenheit)
+    print("Temperature in Kelvin:", kelvin)
+
+elif unit == "F":
+    celsius = (temp - 32) * 5/9
+    kelvin = celsius + 273.15
+
+    print("Temperature in Celsius:", celsius)
+    print("Temperature in Kelvin:", kelvin)
+
+elif unit == "K":
+    celsius = temp - 273.15
+    fahrenheit = (celsius * 9/5) + 32
+
+    print("Temperature in Celsius:", celsius)
+    print("Temperature in Fahrenheit:", fahrenheit)
+
+else:
+    print("Invalid unit entered!")
